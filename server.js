409e4c4fced0003c23b6067f43401fe601ca9a11
alee7510 <83046469+alee7510@users.noreply.github.com>
2021-04-25 15:09:14 -0700

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,37 @@ wss.on('connection', ws => {
     console.log('message: ' + msg.trim());
     wss.clients.forEach((client) => {
       if (client !== ws) {
-        client.send(msg);
+        client.send(data);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('connection closed');
+  });
+});
+'use strict';
+
+const express = require('express');
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
+
+wss.on('connection', ws => {
+  console.log('connection opened');
+
+  ws.on('message', msg => {
+    console.log('message: ' + msg.trim());
+    wss.clients.forEach((client) => {
+      if (client !== ws) {
+        client.send(data);
       }
     });
   });
